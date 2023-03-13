dc5c7e5b686915bd8475573d119f7063d70ce5e2
Wipoo Suksumek <wipoo.suk@gmail.com>
2023-03-13 14:42:40 +0700

    Create ternary.js

---
+++ ternary.js
@@ -0,0 +1,5 @@
+const password = 'pas$word';
+
+password.length >= 12 ? console.log('password is very strong so Welcome!!!') :
+password.lenght >=8 ? console.log('Welcome!!!') :
+console.log('Sorry!!! the password is too short');
